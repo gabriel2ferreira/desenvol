c2aad0594afa58d5976b4b6c431ef6e886d1dfd2
gabriel2ferreira <108493101+gabriel2ferreira@users.noreply.github.com>
2023-05-13 22:39:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ menuBtn.addEventListener('click', () => {
   // Adiciona ou remove a classe "active" das linhas do menu
   menuLines.forEach(line => line.classList.toggle('active'));
 });
+$(document).ready(function() {
+  // Adiciona a classe "active" ao link da página atual no menu
+  var url = window.location.pathname;
+  var filename = url.substring(url.lastIndexOf('/')+1);
+  $('nav a[href="' + filename + '"]').addClass('active');
+
+  // Ativa o menu hamburguer
+  $('.menu-toggle').click(function() {
+    $(this).toggleClass('active');
+    $('nav').toggleClass('active');
+  });
+
+  // Fecha o menu quando um link for clicado
+  $('nav a').click(function() {
+    $('.menu-toggle').removeClass('active');
+    $('nav').removeClass('active');
+  });
+
+  // Ativa o scroll suave
+  $('a[href^="#"]').click(function(e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 1000);
+  });
+});
